366ce9dad6c244816e7739781b275586cb541358
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-26 19:10:26 -0700

    Truly fix badges

--- interface/urls.py
+++ interface/urls.py
@@ -5,7 +5,7 @@ from interface import views
 urlpatterns = [
     url(r'^add/(?P<full_name>(.*))$', views.ProcessRepo, name='process_repo'),
     url(r'^repos$', views.RepoListView.as_view(), name='repo_list'),
-    url(r'^repo/(?P<full_name>[\w|/]+)/badge.svg$', views.BadgeView.as_view(), name='badge'),
+    url(r'^repo/(?P<full_name>[\w|/]+)/badge.svg$', views.BadgeView.as_view(content_type='image/svg+xml'), name='badge'),
     url(r'^repo/(?P<full_name>[\w|/]+)/delete$', views.RepoDeleteView.as_view(), name='delete_repo'),
     url(r'^repo/(?P<full_name>[\w|/]+)$', views.RepoDetailView.as_view(), name='repo_detail'),
     url(r'^build/(?P<pk>[0-9]+)$', views.BuildDetailView.as_view(), name='build_detail'),
