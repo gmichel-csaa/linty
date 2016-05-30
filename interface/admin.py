ac682a577eee561902f8346b71541323e9d46a6c
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-05-30 10:41:07 -0700

    Fix several issues * Add columns to Repo/Build admins * Fix missing context for 404/500

--- interface/admin.py
+++ interface/admin.py
@@ -5,11 +5,21 @@ from interface.models import Build, Repo
 
 @admin.register(Repo)
 class RepoAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'user', 'is_private', 'created_at')
+
     class Meta:
         model = Repo
 
 
 @admin.register(Build)
 class BuildAdmin(admin.ModelAdmin):
+    list_display = ('short_sha', 'ref', 'get_full_name', 'status', 'created_at')
+
+    def get_full_name(self, obj):
+        return obj.repo.full_name
+
+    get_full_name.short_description = 'Repo'
+    get_full_name.admin_order_field = 'repo__full_name'
+
     class Meta:
         model = Build
