c3e9aed5858bd224663e544bc9d082306a60ffe5
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-08-11 17:13:58 -0700

    Add repos to timeline

--- interface/views.py
+++ interface/views.py
@@ -272,7 +272,35 @@ class BadgeView(generic.DetailView):
 
 
 class TimelineView(StaffRequiredMixin, generic.ListView):
-    queryset = Build.objects.all().select_related('repo', 'repo__user').order_by('-created_at')[:500]
+    def get_queryset(self):
+        builds = Build.objects \
+             .select_related('repo') \
+             .order_by('-created_at') \
+             .values('id', 'repo__full_name', 'created_at', 'finished_at')[:500]
+        repos = Repo.objects \
+            .order_by('-created_at') \
+            .values('id', 'full_name', 'created_at')[:500]
+
+        object_list = [
+            {
+                'id': obj['id'],
+                'type': 'build',
+                'repo_name': obj['repo__full_name'],
+                'created_at': obj['created_at'],
+                'finished_at': obj['finished_at']
+            } for obj in builds
+        ]
+        object_list.extend([
+            {
+                'id': obj['id'],
+                'type': 'repo',
+                'full_name': obj['full_name'],
+                'created_at': obj['created_at']
+            } for obj in repos
+        ])
+        object_list.sort(key=lambda i: i['created_at'], reverse=True)
+        return object_list[:500]
+
     template_name = 'interface/timeline.html'
 
 
