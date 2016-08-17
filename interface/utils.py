59b47e4f591d0bc2b207145b8376b4fce9c0b287
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-08-17 11:28:43 -0700

    Restrict JS env vars

--- interface/utils.py
+++ interface/utils.py
@@ -32,10 +32,10 @@ def get_page_number_list(page_num, total_num_pages):
     [12, 13, 14, 15, 16, 17, 18, 19, 20]
     """
     if total_num_pages < 10:
-        return range(1, total_num_pages+1)
+        return range(1, total_num_pages + 1)
     elif page_num <= 5:  # Page to center is below 5, print 9 lowest numbers
         return range(1, 10)
     elif total_num_pages - page_num > 4:  # Enough space on both side of number to center
-        return range(page_num-4, page_num+5)
+        return range(page_num - 4, page_num + 5)
     else:  # Page to center around is at or near the end of the range, print last 9 numbers
-        return range(total_num_pages-8, total_num_pages+1)
+        return range(total_num_pages - 8, total_num_pages + 1)
