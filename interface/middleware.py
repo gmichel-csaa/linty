9c02db0bd14672caa7c97e738076b12cadaf45ad
Brandon Birkholz <brandonjbirkholz@gmail.com>
2016-12-21 10:53:37 -0800

    Redirect to www from apex

--- interface/middleware.py
+++ interface/middleware.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponseRedirect
-
 from interface.models import UserProxy
 
 
@@ -7,18 +5,3 @@ class UserProxyMiddleware(object):
     def process_request(self, request):
         if hasattr(request, 'user') and request.user.is_authenticated():
             request.user.__class__ = UserProxy
-
-
-class SubdomainMiddleware(object):
-    """
-    A middleware class that adds a ``subdomain`` attribute to the current request.
-    """
-
-    def process_request(self, request):
-        """
-        Redirects www to apex
-        """
-        if request.META.get('HTTP_HOST', '').startswith('www'):
-            return HttpResponseRedirect(
-                'https://lintyapp.com/' + request.get_full_path()
-            )
